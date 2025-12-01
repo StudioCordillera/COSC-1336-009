93e8d0e7dd2f68e8a96c7de7a8804afdda0ad4ee
studiocordillera <studiocordillerallc@gmail.com>
2025-12-01 06:03:43 -0600

    Standardize Project_01 final - add requirements header, improve comments, add SOPB/EOPB

--- __CLASS_FILES__/__ASSIGNMENTS__/02_PROJECTS/Project_01/02_FINALS/MJO_Project_1.py
+++ __CLASS_FILES__/__ASSIGNMENTS__/02_PROJECTS/Project_01/02_FINALS/MJO_Project_1.py
@@ -1,67 +1,112 @@
-########################################
-## Matthew Ochoa                   #####
-## October 29, 2025                #####
-## Project: 01                     #####
-## Status: IN PROGRESS             #####
-## Class: COSC 1336                #####
-## --------------------------------#####
-## Project 1 Objectives            #####
-##   4-Exams grade average program #####
-########################################
+###############################################################
+##:::::|   Matthew Ochoa       |-----------------------|:::::##
+##:::::|   October 29, 2025    |   Status: COMPLETE    |:::::##
+##:::::|   Class: COSC 1336    |-----------------------|:::::##
+###############################################################
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+______________________________________________________________
+|                                                             |
+|   REQUIREMENTS                                              |
+|_____________________________________________________________|
+|                                                             |
+|  PROJECT 01 | 4-Exam Grade Average Calculator              |
+|_____________________________________________________________|
+|                                                             |
+|  INPUT:                                                     |
+|    - Exam 1 grade (float)                                   |
+|    - Exam 2 grade (float)                                   |
+|    - Exam 3 grade (float)                                   |
+|    - Exam 4 grade (float)                                   |
+|_____________________________________________________________|
+|                                                             |
+|  PROCESSING:                                                |
+|    - Calculate average of 4 exam grades                     |
+|    - Format average to 2 decimal places                     |
+|_____________________________________________________________|
+|                                                             |
+|  OUTPUT:                                                    |
+|    - Display individual exam grades                         |
+|    - Display calculated average                             |
+|_____________________________________________________________|
+|                                                             |
+|  FUNCTIONS                                                  |
+|_____________________________________________________________|
+|                                                             |
+|    REQUIRED:                                                |
+|        - getFloat(prompt)     | Get float input from user   |
+|        - getCalcAvg()         | Calculate 4-exam average    |
+|        - displaySummary()     | Print formatted results     |
+|        - main()               | ORCHESTRATES                |
+|_____________________________________________________________|
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+###############################################################
+##                      START - MY CODE                      ##
+###############################################################
 
-# Get float value from user given prompt
+#   SOPB
+#  ------------------------------------------------------------------------
+def projectStart():
+    print(' ' * 4, '-'* 80)
+    print(' ' * 4, 'Start of Project 1')
+    print(' ' * 5, 'Written by: Matthew Ochoa')
+    print(' ' * 5,'Date: October 29, 2025')
+    print(' ' * 4,'-' * 80)
+    print('\t  4-Exam Grade Average Calculator')
+    print(' ' * 4,'-' * 80, '\n')
+
+#   EOPB
+#  ------------------------------------------------------------------------
+def projectEnd():
+    print('\n', ' ' * 4,'-' * 80)
+    print(' ' * 5, 'End of Project 1')
+    print(' ' * 4,'-' * 80)
+
+#   Get Float Input
+#  ------------------------------------------------------------------------
 def getFloat(prompt):
-    value=float(input(prompt))
+    value=float(input(prompt))  # Get user input as float
+    return value                # Return validated value >
 
-    return value    
-
-
-# Calc average for 4 exams
+#   Calculate Average
+#  ------------------------------------------------------------------------
 def getCalcAvg(exam1, exam2, exam3, exam4):
-    average=(exam1 + exam2 + exam3 + exam4) / 4
+    average=(exam1 + exam2 + exam3 + exam4) / 4  # Sum all exams / 4
+    return average                                # Return calculated avg >
 
-    return average
-
-# Display summary of all vars
+#   Display Summary
+#  ------------------------------------------------------------------------
 def displaySummary(exam1, exam2, exam3, exam4, examAverage):
-
     print('\n' + '-' * 30)
     print('\n\tSummary of Exam Grades')
     print('\t' + '-' * 30)
-    print('\t  Exam 1: ', exam1)
-    print('\t  Exam 2: ', exam2)
-    print('\t  Exam 3: ', exam3)
-    print('\t  Exam 4: ', exam4)
+    print('\t  Exam 1: ', exam1)           # Display exam 1
+    print('\t  Exam 2: ', exam2)           # Display exam 2
+    print('\t  Exam 3: ', exam3)           # Display exam 3
+    print('\t  Exam 4: ', exam4)           # Display exam 4
     print('\t' + '-' * 30)
-    print('\tExam Average: ', examAverage)
+    print('\tExam Average: ', examAverage)  # Display calculated average
     print('\t' + '-' * 30)
 
-
+###############################################################
+##    MAIN FUNCTION                                          ##
+###############################################################
 def main(): 
-    # Project start
-    print('\n'+'-' * 30)
-    print('\tStart of Project 1')
-    print('\tBy: Matthew Ochoa')
-    
-    print('-' * 30 + '\n')
+    # SOPB
+    projectStart()
 
-    # Get grades from user
+    # Get grades from user | Input collection
     exam1=getFloat('\tEnter your Exam 1 grade: ')
     exam2=getFloat('\tEnter your Exam 2 grade: ')
     exam3=getFloat('\tEnter your Exam 3 grade: ')
     exam4=getFloat('\tEnter your Exam 4 grade: ')
 
-
-    # Call get calculate Average
+    # Calculate average | Process data
     examAverage = format(getCalcAvg(exam1, exam2, exam3, exam4), '.2f')
    
-   
-   # Call display summary
+    # Display summary | Output results
     displaySummary(exam1, exam2, exam3, exam4, examAverage)
    
-    # project end
-    print('\n' + '-' * 30)
-    print('\n\tEnd of Project 1')
-    print('\n' + '-' * 30)
+    # EOPB
+    projectEnd()
       
-main() # calling the function main()
+main() #<= | Call Main
